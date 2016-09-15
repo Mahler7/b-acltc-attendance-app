ed80888b1d38f34bc79b8a8a75cc641921631f40
Justin Jungst <jjungstprogramming@gmail.com>
2016-09-15 15:43:03 -0500

    begun building controller

---
+++ app/controllers/api/v1/attendances_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::AttendancesController < ApplicationController
+  def index
+    @lecture = Lecture.find_by(id: params[:lecture_id])
+    @attendances = @lecture.attendances
+  end
+
+  def create
+    @lecture = Lecture.find_by(id: params[:lecture_id])
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.save
+      render :show
+    else
+      render json: {errors: @attendance.errors.messages}, status: 422 
+    end
+  end
+
+  def show
+    @attendance = Attendance.find_by(id: params[:id])
+    @attendance = Attendance.find_by(id: params[:id])
+  end
+
+  private
+
+    def attendance_params
+      params.require(:attendance).permit(
+        :attended,
+        :lecture_id,
+        :student_id
+      )
+    end
+end
